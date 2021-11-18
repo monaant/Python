0b4cb91f1c6be94decdf73015a4aa791b3249077
monaant <79448034+monaant@users.noreply.github.com>
2021-11-18 23:42:23 +0100

    zadania z pętli while

---
+++ loop-while-LAB1-kurs-RM.py
@@ -0,0 +1,28 @@
+firstRow = 1
+lastRow = 31
+currentRow = firstRow
+
+while currentRow <= lastRow:
+    print("Row number....")
+    currentRow += 1
+
+print("\tzad 2)")
+i = 1 
+imax = 13
+while i <= imax:
+    print(i,"pierwiastek kwadratowy:", i**2, "pierwiastek sześcienny:", i**3)
+    i += 1
+
+print("\tzad 3)")
+x = 0
+xmax = 16
+while x <= xmax:
+    print(2**x)
+    x += 1
+
+print("\tzad 4)")
+y = 1
+ymax = 10
+while y <= ymax:
+    print(y * "x")
+    y += 1
